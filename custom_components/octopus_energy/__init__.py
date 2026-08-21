7682e4077a2b190d8f63a871025a7830960449c7
Mark-RE-Johnson <66674149+Mark-RE-Johnson@users.noreply.github.com>
2026-08-21 20:37:30 +0100

    fix: align cost tracker identity with target (Thanks @Mark-RE-Johnson)
    Co-authored-by: Mark <mark.r.e.johnson@gmail.com>

--- custom_components/octopus_energy/__init__.py
+++ custom_components/octopus_energy/__init__.py
@@ -29,7 +29,7 @@ from .coordinators.heat_pump_configuration_and_status import HeatPumpCoordinator
 from .config.tariff_comparison import async_migrate_tariff_comparison_config
 
 from .config.main import async_migrate_main_config
-from .config.cost_tracker import async_migrate_cost_tracker_config
+from .config.cost_tracker import async_migrate_cost_tracker_config, build_cost_tracker_unique_id
 from .utils import get_active_tariff, get_tariff_parts
 from .utils.debug_overrides import async_get_account_debug_override, async_get_meter_debug_override
 from .utils.error import api_exception_to_string
@@ -117,6 +117,7 @@ async def async_migrate_entry(hass, config_entry):
 
     new_data = dict(config_entry.data)
     title = config_entry.title
+    unique_id = config_entry.unique_id
 
     # Move to reconfiguration from options
     if (config_entry.version <= 8 and new_data is not None and config_entry.options is not None):
@@ -135,10 +136,16 @@ async def async_migrate_entry(hass, config_entry):
           source_device_id=new_data[CONFIG_COST_TRACKER_TARGET_ENTITY_ID],
         )
 
+      if config_entry.version < 11:
+        unique_id = build_cost_tracker_unique_id(
+          new_data[CONFIG_ACCOUNT_ID],
+          new_data[CONFIG_COST_TRACKER_TARGET_ENTITY_ID]
+        )
+
     elif CONFIG_KIND in new_data and new_data[CONFIG_KIND] == CONFIG_KIND_TARIFF_COMPARISON:
       new_data = await async_migrate_tariff_comparison_config(config_entry.version, new_data, hass.config_entries.async_entries)
     
-    hass.config_entries.async_update_entry(config_entry, title=title, data=new_data, options={}, version=CONFIG_VERSION)
+    hass.config_entries.async_update_entry(config_entry, title=title, data=new_data, options={}, unique_id=unique_id, version=CONFIG_VERSION)
 
     _LOGGER.debug("Migration to version %s successful", config_entry.version)
 
